212d644b658d57c2baed0e398ecbe0bc2bd13241
Logan McAnsh <logan@mcan.sh>
2017-06-30 11:55:52 -0400

    use indented sass

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module RailsProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Indented Sass >>
+    config.sass.preferred_syntax = :sass
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
