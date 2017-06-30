33fa0fcb7db2db09f2553f88b7b6d0f530bbdfc4
Logan McAnsh <logan@mcan.sh>
2017-06-30 12:38:49 -0400

    add usersControler and user signup form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user)
+    else
+      redirect_to '/'
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
+  end
+end
