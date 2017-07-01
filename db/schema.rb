4a24ffa8a31dac2eb5208dd5c0dafa2f462ce2d6
Logan McAnsh <logan@mcan.sh>
2017-07-01 14:35:42 -0400

    add ability to make a todo list

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170623234112) do
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
