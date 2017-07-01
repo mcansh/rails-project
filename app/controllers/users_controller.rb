6241dc9f232e62f0c89b4a83184622e0a2d015ef
Logan McAnsh <logan@mcan.sh>
2017-07-01 13:24:32 -0400

    :eyes:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
   def new
     @user = User.new
   end
 
   def create
     user = User.new(user_params)
+
     if user.save
       redirect_to user_path(user)
     else
-      redirect_to '/'
+      redirect_to signup_path
     end
   end
 
   private
+
   def set_user
     @user = User.find(params[:id])
   end
