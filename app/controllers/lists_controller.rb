929939c4e407b8d37ba82efa961777935fe21fc4
Logan McAnsh <logan@mcan.sh>
2017-07-05 14:55:48 -0400

    add task controller as well as the flow for making a new task for a list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @task = @list.tasks.build
   end
 
   def create
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+class TasksController < ApplicationController
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.build(task_params)
+    if @task.save
+      redirect_to @list
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:description)
+    end
+end
