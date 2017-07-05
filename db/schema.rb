4682605236bf6fd92d4a6cd8fd4cfaa75528f85d
Logan McAnsh <logan@mcan.sh>
2017-07-05 13:36:07 -0400

    add task migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705150252) do
+ActiveRecord::Schema.define(version: 20170705171626) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "description"
+    t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
