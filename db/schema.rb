bc6816eff8ca0072dc87f86738faf80e17b5379d
Logan McAnsh <logan@mcan.sh>
2017-07-05 13:13:16 -0400

    set up lists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623234112) do
-  create_table "lists", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170705150252) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "lists", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
