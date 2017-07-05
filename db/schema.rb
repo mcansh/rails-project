a4888a68a9c6f7cb8b2ecda2e102184e452c048e
Logan McAnsh <logan@mcan.sh>
2017-07-05 18:08:49 -0400

    add status to task to define if it is complete or not

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705171626) do
+ActiveRecord::Schema.define(version: 20170705214037) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170705171626) do
     t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status", default: false
   end
 
 end
