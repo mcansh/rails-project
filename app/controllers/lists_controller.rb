10726a8f225a02adcb2c6aec94e7bbec3cee3081
Logan McAnsh <logan@mcan.sh>
2017-07-05 13:13:08 -0400

    set up lists controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,24 @@
+class ListsController < ApplicationController
+  def index
+    @list = List.new
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    list = List.new(list_params)
+    if list.save
+      redirect_to list
+    else
+      redirect_to about_path
+    end
+  end
+
+  private
+    def list_params # strong params
+      params.require(:list).permit(:name)
+    end
+end
