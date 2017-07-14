9a153f424afae4250adc67ca6c37a2015e621bba
Logan McAnsh <logan@mcan.sh>
2017-07-14 15:49:03 -0400

    redirect back to root after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     session[:user_id] = @user.id
+    redirect_to root_path
   end
 
   def destroy
