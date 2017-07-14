76a013f3aa152d707e0cb65483fad4128255cb16
Logan McAnsh <logan@mcan.sh>
2017-07-14 19:29:14 -0400

    add github oauth support

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,30 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email]) || @user = User.find_by(username: [params[:username]])
-    session[:user_id] = @user.id
-    redirect_to root_path
+    if auth_hash = request.env["omniauth.auth"]
+      # store user info
+      oauth_user = request.env["omniauth.auth"]["info"]
+      if user = User.find_by(email: oauth_user["email"])
+        session[:user_id] = user.id
+        redirect_to root_path
+      else
+        user = User.create(email: oauth_user["email"], password: SecureRandom.hex, name: oauth_user["name"], username: oauth_user["nickname"])
+        if user.save
+          session[:user_id] = user.id
+          redirect_to root_path
+        else
+          render 'sessions/new'
+        end
+      end
+    else
+      @user = User.find_by(email: params[:email]) || @user = User.find_by(username: [params[:username]])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        render 'sessions/new'
+      end
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
-
-  resources :lists do
-    resources :tasks
-  end
-
   root 'lists#index'
+  get '/auth/:provider/callback' => 'sessions#create'
   get '/about' => 'site#about'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   get '/login' => 'sessions#new'
   post '/sessions' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
+
+  resources :lists do
+    resources :tasks
+  end
 end
