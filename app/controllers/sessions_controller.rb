cba2f2ecb4676070d60ac85d38f45dbe0c5f175a
Logan McAnsh <logan@mcan.sh>
2017-07-17 12:12:37 -0400

    make a user method to handle oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     if auth_hash = request.env["omniauth.auth"]
-      # store user info
-      oauth_user = request.env["omniauth.auth"]["info"]
-      if user = User.find_by(email: oauth_user["email"])
-        session[:user_id] = user.id
-        redirect_to root_path
-      else
-        user = User.create(email: oauth_user["email"], password: SecureRandom.hex, name: oauth_user["name"], username: oauth_user["nickname"])
-        if user.save
-          session[:user_id] = user.id
-          redirect_to root_path
-        else
-          render 'sessions/new'
-        end
-      end
+      user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = user.id
+      redirect_to root_path
     else
       @user = User.find_by(email: params[:email]) || @user = User.find_by(username: [params[:username]])
       if user && user.authenticate(params[:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   validates :password, presence: true
   validates :username, uniqueness: true
   validates :email, uniqueness: true
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    oauth_user = auth_hash["info"]
+    self.where(email: oauth_user["email"]).first_or_create do |user|
+      user.email = oauth_user["email"]
+      user.password = SecureRandom.hex
+      user.username = oauth_user["nickname"]
+      user.name = oauth_user["name"]
+    end
+  end
 end
