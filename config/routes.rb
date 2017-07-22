3dba14cafa08bff2486202a72fe40678fe432bc2
Logan McAnsh <logan@mcan.sh>
2017-07-21 21:47:57 -0400

    remove unused post routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   root 'lists#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signup' => 'users#new'
-  post '/users' => 'users#create'
   get '/signin' => 'sessions#new'
-  post '/sessions' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
 
   resources :users, only: [:new, :create]
