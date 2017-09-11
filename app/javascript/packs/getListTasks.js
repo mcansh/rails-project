f8024a8180b6090aa6218c65438cbb192918a171
Logan McAnsh <logan@mcan.sh>
2017-09-11 15:04:41 -0400

    update getListTasks

--- app/javascript/packs/getListTasks.js
+++ app/javascript/packs/getListTasks.js
@@ -1,15 +1,10 @@
-import { $ } from './bling';
-
 function getListTasks(listID) {
   const url = `/lists/${listID}.json`;
-  const CSRF = $('meta[name="csrf-token"]').content;
   fetch(url, {
     method: 'GET',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      'X-Requested-With': 'XMLHttpRequest',
-      'X-CSRF-Token': CSRF,
     },
     credentials: 'same-origin',
   })
@@ -17,7 +12,8 @@ function getListTasks(listID) {
     .then((data) => {
       const { tasks } = data;
       tasks.forEach(task => console.log(task));
-    });
+    })
+    .catch(err => console.error(err));
 }
 
 export default getListTasks;
