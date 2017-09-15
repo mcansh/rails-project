9469ea58136d3d9f68905c22aa0910bbfd8c5107
Logan McAnsh <logan@mcan.sh>
2017-09-15 14:32:10 -0400

    destructure 'this'

--- app/javascript/packs/toggleStatus.js
+++ app/javascript/packs/toggleStatus.js
@@ -1,6 +1,6 @@
 function toggleStatus(e) {
   e.preventDefault();
-  const checked = this.checked;
+  const { checked } = this;
   const form = this.closest('form');
   const authenticityToken = form.querySelector('[name="authenticity_token"]').value;
   const body = {
