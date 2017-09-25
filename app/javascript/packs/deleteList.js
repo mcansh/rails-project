f60d191d474021b9e2265c8a35833fbc34df6bd5
Logan McAnsh <logan@mcan.sh>
2017-09-25 19:11:11 -0400

    eslint-ify

--- app/javascript/packs/deleteList.js
+++ app/javascript/packs/deleteList.js
@@ -1,6 +1,7 @@
 function deleteList(e) {
   e.preventDefault();
-  if (confirm('Are you sure you want to delete this list?')) { // eslint-disable-line no-alert, no-restricted-globals
+  // eslint-disable-next-line no-alert, no-restricted-globals
+  if (confirm('Are you sure you want to delete this list?')) {
     this.form.submit();
   }
 }
