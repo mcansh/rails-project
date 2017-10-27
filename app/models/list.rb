849505827946f7c3dde4dea1a1c03f46b3d0794b
Logan McAnsh <logan@mcan.sh>
2017-10-27 11:45:24 -0400

    delete tasks when list is deleted, delete lists when user is deleted

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,8 +1,8 @@
 class List < ApplicationRecord
   validates :name, presence: true
 
-  has_many :tasks
-  has_many :users, through: :lists
+  has_many :tasks, :dependent => :delete_all
+  has_many :users, through: :lists, dependent: :destroy
 
   accepts_nested_attributes_for :tasks
 end
