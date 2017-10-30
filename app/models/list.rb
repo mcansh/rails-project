2bd625b5a5399f13be6708c839f2eea4dfd0a704
Logan McAnsh <logan@mcan.sh>
2017-10-30 17:22:19 -0400

    ¯\_(ツ)_/¯

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
   validates :name, presence: true
 
-  has_many :tasks, :dependent => :delete_all
+  has_many :tasks, :dependent => :destroy
   has_many :users, through: :lists
 
   accepts_nested_attributes_for :tasks
