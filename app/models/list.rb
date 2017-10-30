7f6e43d419293ec08000694a6dc24103e1e4fa26
Logan McAnsh <logan@mcan.sh>
2017-10-30 12:34:38 -0400

    delete tasks when deleting a list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ApplicationRecord
   validates :name, presence: true
 
   has_many :tasks, :dependent => :delete_all
-  has_many :users, through: :lists, dependent: :destroy
+  has_many :users, through: :lists
 
   accepts_nested_attributes_for :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  belongs_to :list, dependent: :destroy
+  belongs_to :list
   belongs_to :user
   validates :description, presence: true
 
