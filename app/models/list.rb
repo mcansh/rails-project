a7c9884fff6f49a77ab277b03d6e4ee0e38a28b5
Logan McAnsh <logan@mcan.sh>
2017-11-06 15:37:37 -0500

    remove

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,8 +3,6 @@ class List < ApplicationRecord
 
   has_many :tasks, dependent: :destroy
 
-  accepts_nested_attributes_for :tasks
-
   def tasks_attributes=(tasks_attributes)
     tasks_attributes.values.each do |task_attribute|
       self.tasks.build(task_attribute)
