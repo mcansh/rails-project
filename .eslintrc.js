fc06b05507ff6d7c6134db7fc3f90b2a74eead5a
Logan McAnsh <logan@mcan.sh>
2018-02-12 14:20:29 -0500

    update eslint config
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,18 @@ module.exports = {
   extends: ['airbnb-base', 'prettier'],
   env: {
     browser: true,
+    es6: true,
   },
   parser: 'babel-eslint',
   plugins: ['import', 'prettier'],
   rules: {
     'no-console': 0,
+        'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true
+      }
+    ]
   },
 };
