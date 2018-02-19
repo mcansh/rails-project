39e3dec2d148ebdde826a9416db43b06f080fcda
Logan McAnsh <logan@mcan.sh>
2018-02-19 16:02:23 -0500

    update error text when args arent supplied

--- app/javascript/packs/patchRequest.js
+++ app/javascript/packs/patchRequest.js
@@ -1,18 +1,22 @@
 const patchRequest = async ({ url, body, authenticityToken }) => {
   if (!url || !body || !authenticityToken) {
-    throw new Error('Must supply a list url, body and Authenticity Token');
+    throw new Error('Must supply a url, body and Authenticity Token');
   }
 
-  await fetch(url, {
-    method: 'PATCH',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      'x-csrf-token': authenticityToken,
-    },
-    credentials: 'same-origin',
-    body: JSON.stringify(body),
-  }).catch(err => console.error(err));
+  try {
+    await fetch(url, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'x-csrf-token': authenticityToken,
+      },
+      credentials: 'same-origin',
+      body: JSON.stringify(body),
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default patchRequest;
